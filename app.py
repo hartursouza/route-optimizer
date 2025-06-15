e6ebc1a751da62da4e2ebc9874b030ea7459851c
Hartur Souza <hartursouza3@gmail.com>
2025-06-15 01:03:26 -0300

    refactor: restructure project by adding __init__ and moving code to a package folder

--- app.py
+++ app.py
@@ -1,34 +1,6 @@
-from flask import Flask, render_template, request, jsonify
-from dotenv import load_dotenv
-import openrouteservice
-import os
+from routes_optimizer import create_app
 
-app = Flask(__name__)
-
-
-load_dotenv()
-
-ORS_API_KEY = os.getenv("ORS_API_KEY") 
-client = openrouteservice.Client(key=ORS_API_KEY)
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/gerar-rota', methods=['POST'])
-def gerar_rota():
-    data = request.json
-    coordenadas = data.get('coordenadas')  # lista de [lon, lat]
-
-    try:
-        rota = client.directions(
-            coordinates=coordenadas,
-            profile='driving-car',
-            format='geojson'
-        )
-        return jsonify(rota)
-    except Exception as e:
-        return jsonify({'erro': str(e)}), 500
+app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ routes_optimizer/static/js/app.js
@@ -0,0 +1,27 @@
+const map = L.map("map").setView([-3.7319, -38.5267], 13); // Fortaleza
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 18,
+}).addTo(map);
+
+const coordenadas = [
+  [-38.5267, -3.7319], // [lon, lat]
+  [-38.52, -3.74],
+  [-38.51, -3.735],
+];
+
+fetch("/route", {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify({ coordenadas }),
+})
+  .then((res) => res.json())
+  .then((data) => {
+    const coords = data.features[0].geometry.coordinates.map((coord) => [
+      coord[1],
+      coord[0],
+    ]);
+    L.polyline(coords, { color: "blue" }).addTo(map);
+    map.fitBounds(L.polyline(coords).getBounds());
+  })
+  .catch((err) => console.error(err));
